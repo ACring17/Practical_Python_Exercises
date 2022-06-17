af3034739cf813e528ef4f9157d8ea622db4289a
Andrew Cring <amcring1@gmail.com>
2022-06-16 19:43:07 -0700

    Working on getting the last line to print

--- game.py
+++ game.py
@@ -6,12 +6,12 @@ guess = int(input("What number am I thinking of between 1 and 100?\n"))
 while n != guess:
     if guess < n:
         print("Guess is too low.")
-        guess = int(input("What number am I thinking of between 1 and 100?\n"))
+        guess = int(input("What number am I thinking of?\n"))
     elif guess > n:
         print("Guess is too high.")
-        guess = int(input("What number am I thinking of between 1 and 100?\n"))
+        guess = int(input("What number am I thinking of?\n"))
     else:
-        print("You got it!")
+        print("You got it! The number was", n)
         break
 
 
